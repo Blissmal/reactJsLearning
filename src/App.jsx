b822e6cae3ce8cc4e4a5a5e60789b78f2f57379d
Blissmal <malutibethuel@gmail.com>
2024-05-17 22:53:16 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Student from "./Student";
 import Card from "./Card";
 import Counter from "./Counter";
 import MyComponent from "./assets/MyComponent";
+import ToDoList from "./ToDoList";
 
 function App() {
   return (
     <>
-      <Student name="Spongebob" age={30}/>
+      {/* <Student name="Spongebob" age={30}/>
       <Card />
       <Counter />
-      <MyComponent />
+      <MyComponent /> */}
+      <ToDoList />
     </>
   );
 }
